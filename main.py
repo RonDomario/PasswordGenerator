16a17266452ed2b422f6be89a0a3cb6faa21aaee
RonDomario <zharkovroman1998@gmail.com>
2025-11-20 23:43:14 +0400

    Fixed password generation. Added PIN generator

--- main.py
+++ main.py
@@ -1,89 +1,130 @@
 from PyQt6 import QtWidgets, QtCore
 import sys
 import pyperclip
-from os.path import join, abspath
-from random import choice, randint
-from stylesheets import label, button, slider, checkbox, browser
-
-
-def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):
-        base_path = sys._MEIPASS
-    else:
-        base_path = abspath(".")
-    return join(base_path, relative_path)
+from stylesheets import label, button, slider, checkbox, browser, lcd
+from secrets import choice, SystemRandom
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.password = ""
+        self.pin = ""
         self.setWindowTitle("PGen")
-        self.setFixedSize(222, 222)
+        self.setFixedSize(200, 400)
         self.central = QtWidgets.QLabel()
         self.central.setStyleSheet(label.style)
         self.setCentralWidget(self.central)
         self.layout = QtWidgets.QVBoxLayout()
         self.central.setLayout(self.layout)
 
-        self.length_label = QtWidgets.QLabel("Length: 1")
-        self.length_label.setStyleSheet(label.style)
-        self.layout.addWidget(self.length_label)
-        self.length_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
-        self.length_slider.setMinimum(1)
-        self.length_slider.setMaximum(50)
-        self.length_slider.setValue(1)
-        self.length_slider.setStyleSheet(slider.style)
-        self.length_slider.valueChanged.connect(self.update_length)
-        self.layout.addWidget(self.length_slider)
+        self.password_label = QtWidgets.QLabel("Password Generator:")
+        self.layout.addWidget(self.password_label)
+        self.password_length_label = QtWidgets.QLabel("Length: 8")
+        self.password_length_label.setStyleSheet(label.style)
+        self.layout.addWidget(self.password_length_label)
+        self.password_length_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.password_length_slider.setMinimum(8)
+        self.password_length_slider.setMaximum(50)
+        self.password_length_slider.setValue(8)
+        self.password_length_slider.setStyleSheet(slider.style)
+        self.password_length_slider.valueChanged.connect(self.update_password_length)
+        self.layout.addWidget(self.password_length_slider)
 
         self.special_switch = QtWidgets.QCheckBox("Special Characters")
         self.special_switch.setStyleSheet(checkbox.style)
         self.layout.addWidget(self.special_switch)
-        self.hide_switch = QtWidgets.QCheckBox("Hide Password")
-        self.hide_switch.setStyleSheet(checkbox.style)
-        self.hide_switch.setCheckState(QtCore.Qt.CheckState.Checked)
-        self.hide_switch.checkStateChanged.connect(self.update_visibility)
-        self.layout.addWidget(self.hide_switch)
-        self.generate_button = QtWidgets.QPushButton("Generate and Copy")
-        self.generate_button.setStyleSheet(button.style)
-        self.generate_button.clicked.connect(self.generate)
-        self.layout.addWidget(self.generate_button)
+        self.password_hide_switch = QtWidgets.QCheckBox("Hide Password")
+        self.password_hide_switch.setStyleSheet(checkbox.style)
+        self.password_hide_switch.setCheckState(QtCore.Qt.CheckState.Checked)
+        self.password_hide_switch.checkStateChanged.connect(self.update_password_visibility)
+        self.layout.addWidget(self.password_hide_switch)
+        self.password_generate_button = QtWidgets.QPushButton("Generate and Copy")
+        self.password_generate_button.setStyleSheet(button.style)
+        self.password_generate_button.clicked.connect(self.generate_password)
+        self.layout.addWidget(self.password_generate_button)
         self.password_browser = QtWidgets.QTextBrowser()
+        self.password_browser.setLineWrapMode(QtWidgets.QTextBrowser.LineWrapMode.FixedColumnWidth)
+        self.password_browser.setLineWrapColumnOrWidth(20)
         self.password_browser.setStyleSheet(browser.style)
         self.layout.addWidget(self.password_browser)
+
+        self.pin_label = QtWidgets.QLabel("PIN Generator:")
+        self.pin_label.setStyleSheet(label.style)
+        self.layout.addWidget(self.pin_label)
+        self.pin_length_label = QtWidgets.QLabel("Length: 4")
+        self.pin_length_label.setStyleSheet(label.style)
+        self.layout.addWidget(self.pin_length_label)
+        self.pin_length_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.pin_length_slider.setMinimum(4)
+        self.pin_length_slider.setMaximum(12)
+        self.pin_length_slider.setValue(4)
+        self.pin_length_slider.setStyleSheet(slider.style)
+        self.pin_length_slider.valueChanged.connect(self.update_pin_length)
+        self.layout.addWidget(self.pin_length_slider)
+        self.pin_hide_switch = QtWidgets.QCheckBox("Hide PIN")
+        self.pin_hide_switch.setStyleSheet(checkbox.style)
+        self.pin_hide_switch.setCheckState(QtCore.Qt.CheckState.Checked)
+        self.pin_hide_switch.checkStateChanged.connect(self.update_pin_visibility)
+        self.layout.addWidget(self.pin_hide_switch)
+        self.pin_generate_button = QtWidgets.QPushButton("Generate and Copy")
+        self.pin_generate_button.setStyleSheet(button.style)
+        self.pin_generate_button.clicked.connect(self.generate_pin)
+        self.layout.addWidget(self.pin_generate_button)
+        self.pin_display = QtWidgets.QLCDNumber()
+        self.pin_display.setSegmentStyle(QtWidgets.QLCDNumber.SegmentStyle.Flat)
+        self.pin_display.display("")
+        self.pin_display.setNumDigits(4)
+        self.pin_display.setStyleSheet(lcd.style)
+        self.layout.addWidget(self.pin_display)
         self.show()
 
-    def update_length(self, value):
-        self.length_label.setText(f"Length: {value}")
+    def update_password_length(self, value):
+        self.password_length_label.setText(f"Length: {value}")
 
-    def update_visibility(self):
-        if self.hide_switch.isChecked():
+    def update_pin_length(self, value):
+        self.pin_length_label.setText(f"Length: {value}")
+
+    def update_password_visibility(self):
+        if self.password_hide_switch.isChecked():
             self.password_browser.setText("*" * len(self.password))
         else:
             self.password_browser.setText(self.password)
 
-    def generate(self):
-        characters = []
-        alpha = "abcdefghijklmnopqrstuvwxyz"
-        numbers = "0123456789"
-        special = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
-        collection = alpha + numbers
+    def update_pin_visibility(self):
+        if self.pin_hide_switch.isChecked():
+            self.pin_display.display("P" * len(self.pin))
+        else:
+            self.pin_display.display(self.pin)
+
+    def generate_password(self):
+        password = [choice(ascii_lowercase), choice(ascii_uppercase), choice(digits)]
+        collection = ascii_lowercase + ascii_uppercase + digits
         if self.special_switch.isChecked():
-            collection += special
-        length = self.length_slider.value()
-        for _ in range(length):
-            char = choice(collection)
-            if char.isalpha():
-                if randint(0, 1):
-                    char = char.upper()
-            characters.append(char)
-        self.password = "".join(characters)
-        if self.hide_switch.isChecked():
+            collection += punctuation
+            password.append(choice(punctuation))
+        length = self.password_length_slider.value()
+        password += [choice(collection) for _ in range(length - 3 - self.special_switch.isChecked())]
+        SystemRandom().shuffle(password)
+        self.password = "".join(password)
+        if self.password_hide_switch.isChecked():
             self.password_browser.setText("*" * len(self.password))
         else:
             self.password_browser.setText(self.password)
         pyperclip.copy(self.password)
+
+    def generate_pin(self):
+        length = self.pin_length_slider.value()
+        pin = [choice(digits) for _ in range(length)]
+        SystemRandom().shuffle(pin)
+        self.pin = "".join(pin)
+        self.pin_display.setNumDigits(length)
+        if self.pin_hide_switch.isChecked():
+            self.pin_display.display("P" * len(self.pin))
+        else:
+            self.pin_display.display(self.pin)
+        pyperclip.copy(self.pin)
 
 
 if __name__ == '__main__':
---
+++ stylesheets/lcd.py
@@ -0,0 +1,9 @@
+from .colors import *
+
+style = """
+            QLCDNumber {
+                background-color: %s;
+                color: %s;
+                border: %s;
+            }
+        """ % (normal_background, normal_color, border)
